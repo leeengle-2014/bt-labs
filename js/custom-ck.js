f7ef0dd8e92329dd328127bb4bcdbdfba8411387
Dan Brown <slightlyoffbeat@gmail.com>
2013-08-26 18:54:56 -0700

    integrating wyatt's changes into LESS, etc

--- js/custom-ck.js
+++
@@ -1,4 +0,0 @@
-// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
-// IT'S ALL JUST JUNK FOR OUR DOCS!
-// ++++++++++++++++++++++++++++++++++++++++++
-!function(e){e(function(){var t=e(window);window.prettyPrint&&prettyPrint()});e(".menu li a").click(function(t){var n=e(this);n.hasClass("active")||n.addClass("active");t.preventDefault()})}(window.jQuery);
